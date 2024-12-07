50c5fe6d834f2ec8ecbef887a90da5a24f2377c3
Stephen Mizell <smizell@gmail.com>
2024-12-07 11:02:26 -0600

    Refactor

--- day07.py
+++ day07.py
@@ -1,51 +1,37 @@
 import operator
 
-ops = {"add": operator.add, "mul": operator.mul, "concat": lambda a, b: int(f"{a}{b}")}
-
 
 def part1(input_file):
     equations = load_equations(input_file)
     true_equations = []
     for total, numbers in equations:
-        if solve1(total, numbers):
+        if solve(total, numbers, [operator.add, operator.mul]):
             true_equations.append(total)
     return sum(true_equations)
 
 
-# 6042069247243 is too low
 def part2(input_file):
     equations = load_equations(input_file)
     true_equations = []
     for total, numbers in equations:
-        if solve2(total, numbers):
+        if solve(total, numbers, [operator.add, operator.mul, concat_number]):
             true_equations.append(total)
     return sum(true_equations)
 
 
-def solve1(fin_total, numbers, cur_total=0):
+def concat_number(a, b):
+    return int(f"{a}{b}")
+
+
+def solve(fin_total, numbers, ops, cur_total=0):
     if not numbers:
         return False
     number, *rest_numbers = numbers
-    ops = [operator.add, operator.mul]
     for op in ops:
         new_total = op(cur_total, number)
         if not rest_numbers and new_total == fin_total:
             return True
-        if solve1(fin_total, rest_numbers, new_total):
-            return True
-    return False
-
-
-def solve2(fin_total, numbers, cur_total=0):
-    if not numbers:
-        return False
-    number, *rest_numbers = numbers
-    ops = [operator.add, operator.mul, lambda a, b: int(f"{a}{b}")]
-    for op in ops:
-        new_total = op(cur_total, number)
-        if not rest_numbers and new_total == fin_total:
-            return True
-        if solve2(fin_total, rest_numbers, new_total):
+        if solve(fin_total, rest_numbers, ops, new_total):
             return True
     return False
 
