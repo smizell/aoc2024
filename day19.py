8508fcebbebc1a49069a150a3eef4fa40648accd
Stephen Mizell <smizell@gmail.com>
2024-12-19 09:28:14 -0600

    Day 19 Part 2

--- day19.py
+++ day19.py
@@ -18,6 +18,23 @@ def part1(input_file):
     return len(possible)
 
 
+def part2(input_file):
+    patterns, designs = load_data(input_file)
+
+    @cache
+    def design_counter(design):
+        total = 0
+        if design == "":
+            return 1
+        max_pattern_len = max(map(len, patterns))
+        for i in range(min(len(design), max_pattern_len) + 1):
+            if design[:i] in patterns:
+                total += design_counter(design[i:])
+        return total
+
+    return sum([design_counter(design) for design in designs])
+
+
 def load_data(input_file):
     with open(input_file) as f:
         raw_patterns, raw_designs = f.read().split("\n\n")
