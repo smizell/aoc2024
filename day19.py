f78f1dda3f568f518408653393e97dcbf09880c4
Stephen Mizell <smizell@gmail.com>
2024-12-19 09:23:35 -0600

    Day 19 Part 1

---
+++ day19.py
@@ -0,0 +1,26 @@
+from functools import cache
+
+
+def part1(input_file):
+    patterns, designs = load_data(input_file)
+
+    @cache
+    def design_is_possible(design):
+        if design == "":
+            return True
+        max_pattern_len = max(map(len, patterns))
+        for i in range(min(len(design), max_pattern_len) + 1):
+            if design[:i] in patterns and design_is_possible(design[i:]):
+                return True
+        return False
+
+    impossible = [design for design in designs if design_is_possible(design)]
+    return len(impossible)
+
+
+def load_data(input_file):
+    with open(input_file) as f:
+        raw_patterns, raw_designs = f.read().split("\n\n")
+    patterns = raw_patterns.split(", ")
+    designs = raw_designs.split("\n")
+    return patterns, designs
