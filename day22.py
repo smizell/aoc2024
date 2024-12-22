e4faf923eae80d0183f9ce90c9c8557465de4666
Stephen Mizell <smizell@gmail.com>
2024-12-22 09:42:25 -0600

    Day 22 Part 2 not working

--- day22.py
+++ day22.py
@@ -1,11 +1,27 @@
-from itertools import cycle
-
-
 def part1(input_file):
     secret_numbers = load_secret_numbers(input_file)
     for _ in range(2000):
         secret_numbers = map(process, secret_numbers)
     return sum(secret_numbers)
+
+
+def part2(input_file):
+    secret_number = 123
+    sequence = [secret_number]
+    for _ in range(10 - 1):
+        secret_number = process(secret_number)
+        sequence.append(secret_number)
+    prices = []
+    for num in sequence:
+        prices.append(int(str(num)[-1]))
+    price_diffs = []
+    for i in range(len(prices) - 1):
+        price_diffs.append(prices[i + 1] - prices[i])
+    return max_index_after(prices, 4), prices, price_diffs
+
+
+def max_index_after(values, after):
+    return max(enumerate(values[after:]), key=lambda n: n[1])[0] + after
 
 
 def mix(a, b):
